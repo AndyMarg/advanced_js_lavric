c676f8bb233a98d71a08c5b73192105152ad3e71
AndyMarg <margashov@gmail.com>
2019-09-22 11:38:44 +0300

    Home 1 patch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "@babel/polyfill";
+
 // import './js/lesson_1';
 import './js/home_1';
--- src/js/home_1/stringUtils.js
+++ src/js/home_1/stringUtils.js
@@ -8,10 +8,10 @@
 }
 
 function* getWords(str) {
-    str = str.replace(/\s+/g, ' ').trim();
+    str = str.replace(/\s+/g, ' ').trim() + ' ';
     let start = 0, pos = 0;
 
-    while ((pos = str.indexOf(' ', pos)) != -1) {
+    while ((pos = str.indexOf(' ', pos)) !== -1) {
         yield str.substring(start, pos);
         start = ++pos;
     }
